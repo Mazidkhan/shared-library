22623149835c3508522bb81cd46fc3eebbdca6a9
Mazidkhan <mazidula.mk@gmail.com>
2024-04-05 20:16:10 +0530

    Update msg.groovy

--- vars/msg.groovy
+++ vars/msg.groovy
@@ -1,11 +1,12 @@
 import java.util.Properties
+
 def caller() {
     def configProperties = new Properties()
     def classLoader = this.class.classLoader
     def configStream = classLoader.getResourceAsStream("resources/config.properties")
     if (configStream != null) {
         configProperties.load(configStream)
-        def channel=configProperties.getProperty('SLACK_CHANNEL_NAME')
+        def channel = configProperties.getProperty('SLACK_CHANNEL_NAME')
         println("hello $channel")
     } else {
         println("Hello from shared library! (config.properties not found)")
