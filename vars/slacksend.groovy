a17f4bab46e40a2ea606d4192adc8c0db2c8cb3c
Mazidkhan <mazidula.mk@gmail.com>
2024-04-05 20:53:21 +0530

    Update slacksend.groovy

--- vars/slacksend.groovy
+++ vars/slacksend.groovy
@@ -1,3 +1,9 @@
-def call () {
-    slackSend channel: 'jenkins', color: 'good', message: 'Job run failed', teamDomain: 'practice-k979999', username: 'rahil khan'
+import java.util.Properties
+
+def call(String configFilePath) {
+    def properties = new Properties()
+    properties.load(new File(configFilePath).newDataInputStream())
+    def channel=properties.getProperty("SLACK_CHANNEL_NAME")
+    def message=properties.getProperty("ACTION_MESSAGE")
+    slackSend channel: channel, color: 'good', message: message, teamDomain: 'practice-k979999', username: 'rahil khan'
 }
