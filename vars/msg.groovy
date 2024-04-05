a64d11e7f3b98fce37baa3b658b4f93dc515c703
Mazidkhan <mazidula.mk@gmail.com>
2024-04-05 20:47:42 +0530

    Update msg.groovy

--- vars/msg.groovy
+++ vars/msg.groovy
@@ -3,5 +3,6 @@ import java.util.Properties
 def call(String configFilePath) {
     def properties = new Properties()
     properties.load(new File(configFilePath).newDataInputStream())
-    return properties.getProperty("username")
+    def name=properties.getProperty("username")
+    println(name)
 }
