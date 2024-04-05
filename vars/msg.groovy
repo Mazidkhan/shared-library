8fc047a80380b7d1e05e3837d94652347ce7c556
Mazidkhan <mazidula.mk@gmail.com>
2024-04-05 20:13:38 +0530

    Update msg.groovy

--- vars/msg.groovy
+++ vars/msg.groovy
@@ -2,7 +2,7 @@ import java.util.Properties
 def caller() {
     def configProperties = new Properties()
     def classLoader = this.class.classLoader
-    def configStream = classLoader.getResourceAsStream("config.properties")
+    def configStream = classLoader.getResourceAsStream("resources/config.properties")
     if (configStream != null) {
         configProperties.load(configStream)
         def channel=configProperties.getProperty('SLACK_CHANNEL_NAME')
